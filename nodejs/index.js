814d1293e6b73a8810435611c49a52a6033c3545
Stephen Connolly <14976148+stephenconnolly1@users.noreply.github.com>
2019-12-23 22:07:41 +0000

    Add travis build number. Tidy up HTML rendition of build metadata

--- nodejs/index.js
+++ nodejs/index.js
@@ -4,9 +4,12 @@ var os = require('os')
 var http = require('http')
 
 function handleRequest(req, res) {
-  console.log('Request')
-  res.write('Hi there! I\'m being served from ' + os.hostname())
-  res.write('AMI built from commit' + process.env.TRAVIS_COMMIT )
+  console.log('Request. Version:' + process.env.TRAVIS_BUILD_NUMBER)
+  res.write('<HTML><BODY>')
+  res.write('<P>Hi there! I\'m being served from ' + os.hostname() + '</P>')
+  res.write('<P>AMI built from commit' + process.env.TRAVIS_COMMIT + '</P>')
+  res.write('<P>Travis build numberc' + process.env.TRAVIS_BUILD_NUMBER + '</P>')
+  res.write('</BODY></HTML>')
   res.end()
 }
 
